f3929eec84687f397a1bdc83e409a74a0cc4e620
hastamanana <who.am.y@mail.ru>
2025-03-02 22:52:21 +0300

    Upgrade задания 1. Автоматическое определние min / max диапазона 2. Кол-во попыток дается в зависимости от кол-ва чисел в диапазоне 3. Выполнено TODO

--- main_2.py
+++ main_2.py
@@ -1,7 +1,18 @@
-start, end = map(int, input().split())
+import math
+
+
+# добавил автоматическое определние мин. и макс. значений 
+range_nums = list(map(int, input().split()))
+start = min(range_nums)
+end = max(range_nums)
 
 assert start < end, "Укажите корректный диапазон"
-max_attempt: int = 7
+
+# добавил кол-во попыток в зависимости от диапазона
+all_nums_in_range = end - start
+max_attempt: int = math.ceil(math.log(all_nums_in_range, 2)) + 1
+# вывод кол-ва попыток для указанного диапазона (опционально)
+print(max_attempt)
 
 attempt: int = 0
 answers: tuple[str, ...] = ("угадал", "больше", "меньше")
@@ -10,27 +21,30 @@ while attempt < max_attempt:
     middle: int = (start + end) // 2
     # TODO: надо обработать вывод,
     #  чтобы не выводилось предполагаемое число
+
+    #todo сделал
     print(middle)
+    ans: str = None
+    while ans not in answers:
+        ans: str = input()
+        if ans not in answers:
+            print("Возможные ответы:", answers)
+            continue
+    else:
+        attempt += 1
 
-    ans: str = input()
-    if ans not in answers:
-        print("Возможные ответы,", answers)
-        continue
+        if ans == "угадал":
+            print(
+                f"Я угадал! Загаданное число {middle}. "
+                f"Количество попыток: {attempt}"
+            )
+            break
 
-    attempt += 1
+        if ans == "больше":
+            start = middle + 1
+        elif ans == "меньше":
+            end = middle - 1
 
-    if ans == "угадал":
-        print(
-            f"Я угадал! Загаданное число {middle}. "
-            f"Количество попыток: {attempt}"
-        )
-        break
-
-    if ans == "больше":
-        start = middle + 1
-    elif ans == "меньше":
-        end = middle - 1
-
-    if start > end:
-        print("Ошибка! Похоже, вы дали противоречивые ответы.")
-        break
+        if start > end:
+            print("Ошибка! Похоже, вы дали противоречивые ответы.")
+            break
