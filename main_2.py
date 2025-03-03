31bded899a83e32b92d4a20ad90cd68915498126
hastamanana <who.am.y@mail.ru>
2025-03-03 18:23:27 +0300

    Сделал декомпозицию

--- main_2.py
+++ main_2.py
@@ -1,50 +1,81 @@
 import math
 
 
-# добавил автоматическое определние мин. и макс. значений 
-range_nums = list(map(int, input().split()))
-
-start = min(range_nums)
-end = max(range_nums)
-
-assert start != end, "Укажите корректный диапазон"
-
-# добавил кол-во попыток в зависимости от диапазона
-all_nums_in_range = end - start
-max_attempt: int = int(math.log2(all_nums_in_range)) + 1
-# вывод кол-ва попыток для указанного диапазона (опционально)
-print(max_attempt)
-
-attempt: int = 0
-answers: tuple[str, ...] = ("угадал", "больше", "меньше")
-
-while attempt < max_attempt:
-    middle: int = (start + end) // 2
-    # TODO: надо обработать вывод,
-    #  чтобы не выводилось предполагаемое число
-
-    #todo сделал
-    print(middle)
-    ans: str = input()
-    while ans not in answers:
-        print("Возможные ответы:", answers)
-        ans: str = input()
+def range_2_numbers():
+    while True:
+        try:
+            range_nums = list(map(int, input('Введите 2 неравных числа через пробел: ').split()))
             
-    
-    attempt += 1
+        except ValueError:
+            print('Нужно ввести 2 неравных числа через пробел! Попробуйте еще раз')
+            continue
 
-    if ans == "угадал":
-        print(
-            f"Я угадал! Загаданное число {middle}. "
-            f"Количество попыток: {attempt}"
-        )
+        else:
+
+            if len(range_nums) != 2:
+                print('Необходимо ввести ровно 2 числа через пробел. Попробуйте еще раз')
+                continue
+            
+            if range_nums[0] == range_nums[1]:
+                print('Числа должны быть неравными. Попробуйте еще раз.')
+                continue
+
+            if range_nums[0] < 1 or range_nums[1] < 1:
+                print("Оба числа должны быть больше 0. Попробуйте еще раз")
+                continue
+
         break
 
-    if ans == "больше":
-        start = middle + 1
-    elif ans == "меньше":
-        end = middle - 1
+    start_range = min(range_nums)
+    end_range = max(range_nums)
 
-    if start > end:
-        print("Ошибка! Похоже, вы дали противоречивые ответы.")
-        break
+    return start_range, end_range
+
+
+def count_attempts(start_num, end_num):
+    diff = end_num - start_num
+    return int(math.log2(diff)) + 1
+
+def game(start, end, max_attempt):
+    attempt: int = 0
+    answers: tuple[str, ...] = ("угадал", "больше", "меньше")
+
+    while attempt < max_attempt:
+        middle: int = (start + end) // 2
+        print(middle)
+        ans: str = input()
+        while ans not in answers:
+            print("Возможные ответы:", answers)
+            ans: str = input()
+                
+        
+        attempt += 1
+
+        if ans == "угадал":
+            print(
+                f"Я угадал! Загаданное число {middle}. "
+                f"Количество попыток: {attempt}"
+            )
+            break
+
+        if ans == "больше":
+            start = middle + 1
+        elif ans == "меньше":
+            end = middle - 1
+
+        if start > end:
+            print("Ошибка! Похоже, вы дали противоречивые ответы.")
+            break
+
+
+
+
+
+def main():
+    start, end = range_2_numbers()
+    max_attempt = count_attempts(start, end)
+    game(start, end, max_attempt)
+
+
+if __name__ == '__main__':
+    main()
